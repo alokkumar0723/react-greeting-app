67b60db7d8eea195939e80f29c14df96ecb4d2ab
alokkumar0723 <alokkumar0723@gmail.com>
2022-07-20 00:23:37 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+//#17 video->React Js Mini-Project => Create a Simple Greeting Website using ReactJS
+
+//1. Create a react app from scratch
+//2. Show a heading that says Hello sir, Good Morning,
+//   if time is between 1AM to 11 AM
+//3. Good Afternoon, if 12PM to 7PM
+//4. Good Night, if 7PM till Midnight
+//5. Apply CSS in it, then dynamically change the color
+//   Of Greeting part only using Inline CSS style.
+//   Ex.Gree when its Morning time ,Orange when its Noon/evening time ,
+//   Black when its Nigh time 
+
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import './index.css'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <App/>,
+  
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
